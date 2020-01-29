ceee30d2455179605b65a2eb312c625eed21e118
Informagier <24705092+Informagier@users.noreply.github.com>
2020-01-30 00:19:28 +0100

    Fix the ReadJSON method

--- src/main/scala/Controller.scala
+++ src/main/scala/Controller.scala
@@ -6,8 +6,51 @@ import Main.getClass
 
 import scala.io.Codec
 
+import upickle.default._
+import upickle.default.{ReadWriter => RW, macroRW}
+import upickle.implicits.key
+
+import ujson.{Obj, Arr, Value}
+
+final private case class BerichtJson(content: Map[String, List[Bericht]])
+private object BerichtJson{
+  def marshall(b: BerichtJson): Value = {
+    Obj.from(b.content.mapValues(x => Arr(x.map(Bericht.marshall))))
+  }
+  def unmarshall(v : Value) : BerichtJson = {
+    BerichtJson(v.obj.mapValues(_.arr.map(Bericht.unmarshall).toList).toMap)
+  }
+  implicit val rw: RW[BerichtJson] = readwriter[Value].bimap[BerichtJson](marshall, unmarshall)
+}
+private final case class Bericht( bezirke: List[String] = List(), 
+                                  date: String = "",
+                                  content: String = "",
+                                  number: String = "",
+                                  title: String = "")
+private object Bericht{
+  def marshall(b :Bericht): Value = {
+    Obj(
+        "Bezirke" -> Arr(b.bezirke),
+        "Datum"   -> b.date,
+        "Meldung" -> b.content,
+        "Nummer"  -> b.number,
+        "Titel"   -> b.title
+    )
+  }
+  def unmarshall(v: Value): Bericht = {
+    val b = v.obj
+    Bericht(b("Bezirke").arr.map(_.str).toList,
+            b("Datum").strOpt.getOrElse(""),
+            b("Meldung").strOpt.getOrElse(""),
+            b("Nummer").strOpt.getOrElse(""),
+            b("Titel").strOpt.getOrElse("")
+    )
+  }
+  implicit val rw: RW[Bericht] = readwriter[Value].bimap[Bericht](marshall, unmarshall)
+}
+
 class Controller {
-  private var invertedIndex : InvertedIndex[Int, String, Any] = InvertedIndex()
+  private var invertedIndex : InvertedIndex[String, String, Any] = InvertedIndex()
 
   def search(query: String): List[String] = {
     println("Suchanfrage: " + query)
@@ -24,30 +67,36 @@ class Controller {
     result
   }
 
+  
   def createInverseIndexJSON(file:String)={
+
     val t0 = System.nanoTime()
 
-    val CR = 0x0D.toChar
-    val LF = 0x0A.toChar
-    val CRLF = ""+CR+ CR + LF
+    val input = new FileInputStream(file)
 
+    val content = read[BerichtJson](input)
 
-    val src = scala.io.Source.fromInputStream( getClass.getResourceAsStream(file ) )(Codec("ISO-8859-1"))
-    val lines = src.mkString.split(CRLF)
-    val numPattern = "Nr.[ ]{0,1}[0-9]{2,}".r
+    println("Read after: " + (System.nanoTime() - t0)/1e+6/1000  + "sec")
 
-    val docFeatures = lines.map(line => { 
-      val idString = numPattern.findFirstIn(line).toString.replaceAll("[^0-9]", "")
-      val id = if(idString != "") Integer.parseInt(idString) else -1
-      val content = transform(line).map((_, null))
-      (id, content)
+    val indexContent = content.content.mapValues(berichts => {
+      val bericht = berichts.headOption.getOrElse(Bericht())
+      val berichtContent = Iterable(bericht.content,
+                                    bericht.title,
+                                    bericht.number,
+                                    bericht.date) 
+                            .concat(bericht.bezirke)
+      val berichtTokens = berichtContent.flatMap(transform)
+                                        .map((_, null))
+      berichtTokens.toSet
     })
-    invertedIndex = InvertedIndex(docFeatures: _*) 
 
-    val t1 = System.nanoTime()
-    println("Elapsed time: " + (t1 - t0)/1e+6/1000  + "sek")
-    this.invertedIndex=invertedIndex
+    println("Tokenized after: " + (System.nanoTime() - t0)/1e+6/1000  + "sec")
 
+    invertedIndex = InvertedIndex(indexContent.toSeq: _*) 
+
+    println("Indexed after: " + (System.nanoTime() - t0)/1e+6/1000  + "sec")
+
+    println("Elapsed time: " + (System.nanoTime() - t0)/1e+6/1000  + "sec")
 
   }
 
@@ -67,7 +116,7 @@ class Controller {
   def loadInverseIndex(file:String) = {
     val ois = new ObjectInputStream(new FileInputStream(file))
     try {
-      val index = ois.readObject.asInstanceOf[Map[String, Set[(Int, Any)]]]
+      val index = ois.readObject.asInstanceOf[Map[String, Set[(String, Any)]]]
       this.invertedIndex = InvertedIndex(index)
     } finally {
       ois.close()
