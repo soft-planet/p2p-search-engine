4e7f4d84b72fa39abe93166af3d72dbeba88ca93
Holger Godglück <s0561314@htw-berlin.de>
2020-01-30 16:24:47 +0100

    Main argument only Year

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -6,13 +6,13 @@ import java.io.File
 
 
 object Main {
-  // argument ist src/main/resources/tabelle201X.json, X = 6,7,8 oder 9
+  // argument ist 201X, X = 6,7,8 oder 9
   def main(args: Array[String]) {
 
     val arglist = args.toList
     val iFile="inverseIndex.bin"
     val c=new Controller()
-    c.createInverseIndexJSON( arglist(0))//"data" + File.separator + "tabelle2017.json")
+    c.createInverseIndexJSON( "src/main/resources/tabelle" +arglist(0) + ".json")//"data" + File.separator + "tabelle2017.json")
     c.saveInverseIndex(iFile)
 
 
